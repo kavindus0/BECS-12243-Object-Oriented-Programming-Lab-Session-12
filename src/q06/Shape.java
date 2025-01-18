e16fcfb8b2b0a2345a18f74e095864402552f214
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-19 00:15:25 +0530

    shape abstract class created

---
+++ src/q06/Shape.java
@@ -0,0 +1,15 @@
+package q06;
+
+public abstract class Shape {
+
+    String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public abstract void fitText(String n);
+
+
+
+}
