22b423930b0f8c64fa6cdc83ddf09be53f2fe1fb
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-18 20:24:17 +0530

    main class created

---
+++ src/q03/main.java
@@ -0,0 +1,19 @@
+package q03;
+
+public class main {
+    public static void main(String[] args) {
+//        Ball ball = new Football("spalding");
+//        Ball ball = new Football("Spalding"); Baseball baseball = (Baseball)ball;
+
+//Object obj = new Baseball("spalding");
+
+//Object obj = new Baseball("spalding"); Tossable tossable = obj;
+
+
+//Tossable tossable = new Baseball("spalding"); Object objtossable;
+
+//Rock rock new Baseball("spalding");
+
+Tossable tossable = new Rock();
+    }
+}
