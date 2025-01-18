77179e07f224c28ba58e60e96cb474eb787efb51
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-19 00:14:46 +0530

    Circle class created

---
+++ src/q06/Circle.java
@@ -0,0 +1,19 @@
+package q06;
+
+public class Circle extends Shape{
+    int radius;
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public void fitText(String n) {
+        if (n.length() > this.radius) {
+            System.out.println("Text too long");
+        } else {
+            System.out.println("Text fits");
+        }
+    }
+}
