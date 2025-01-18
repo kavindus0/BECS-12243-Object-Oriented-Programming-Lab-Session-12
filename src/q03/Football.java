2f0e6fb916239465110807c160f143574289c0a7
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-18 20:23:33 +0530

    class created

---
+++ src/q03/Football.java
@@ -0,0 +1,17 @@
+package q03;
+
+public class Football extends Ball {
+    public Football(String brandName) {
+        super(brandName);
+    }
+
+    @Override
+    void bounce() {
+
+    }
+
+    @Override
+    public void Toss() {
+
+    }
+}
