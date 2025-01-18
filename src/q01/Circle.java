b382d1d9f483b3fa990d20963838ab7b8805e557
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-18 18:34:37 +0530

    class created

---
+++ src/q01/Circle.java
@@ -0,0 +1,41 @@
+package q01;
+
+public class Circle {
+
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, double radius) {
+        this.color = color;
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+// get radi
+
+    public String toString() {
+        return "Circle[radius=r,color=c]";
+    }
+}
