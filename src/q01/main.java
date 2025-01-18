9043c20f8f8403f7d652c7de2d450e6799b379a6
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-18 18:54:08 +0530

    uncommented

--- src/q01/main.java
+++ src/q01/main.java
@@ -3,11 +3,20 @@ package q01;
 public class main {
     public static void main(String[] args) {
         Circle c1 = new Circle();
+
 Circle c2 = new Cylinder();
+
 Cylinder c3 = new Cylinder (3.1);
+
 Circle c4 = new Circle(2.3);
+
 Circle c5 = new Cylinder (2.3, 4.0);
+
 Cylinder c6 = new Cylinder (3.1, 5.2, "red");
-//Circle c7 = new Circle(2.1, "yellow");
+
+Circle c7 = new Circle(2.1, "yellow");
+
+
     }
+
 }
