537d12aa94d3fe1e02a830546134df0f7742a473
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-19 20:12:30 +0530

    mainApp Created

---
+++ src/SLIIT_2019/FoodDemo.java
@@ -0,0 +1,14 @@
+package SLIIT_2019;
+
+public class FoodDemo {
+
+public static void main(String[] args) {
+
+Item [] items = new Item[]{new Rice(5),new Dhal(3),
+        new Soya(4),new Rice(4),new Soya(4)};
+
+Food food= new Food(items);
+food.calculateCost();
+}
+
+}
